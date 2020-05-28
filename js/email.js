0c632543565893dc6f3c525c62d5d4bb24cb3496
nibir <biplob.asanibir@gmail.com>
2020-05-28 23:29:23 +0600

    70% complete

---
+++ js/email.js
@@ -0,0 +1,18 @@
+const sentEmail = () =>{
+    const sender_mail = document.getElementById("senderEmail").value
+    const sender_name = document.getElementById("senderName").value
+    const mail_subject = document.getElementById("Subject").value
+    const mail_body = document.getElementById("mailBody").value
+
+    if( sender_mail && mail_body ) {
+        Email.send({
+            SecureToken : "2728028c-8ae6-4d53-a734-bf1605a41a0b",
+            To : "biplob.asanibir@gmail.com",
+            From : sender_mail,
+            Subject : mail_subject,
+            Body : "Name : " +sender_name + " \n\n\n "+ mail_body
+        }).then(
+          message => alert(message)
+        );
+    }
+}
